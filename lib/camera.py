cbed4df98dc62befe03001ac0abb3cb3a397c797
Mike Anderson <hello@mrmikea.com>
2015-08-22 00:47:32 -0500

    Give Camera a look target

--- lib/camera.py
+++ lib/camera.py
@@ -4,7 +4,12 @@ from vec3 import vec3
 class Camera:
     def __init__(self, pos=vec3(0, 0, 0)):
         self.pos = pos
+        self.looktarget = pos
+
+    def lookat(self, target):
+        self.looktarget = target
 
     def log(self):
-        print("<Camera pos=[%s, %s, %s]>" %
-              (self.pos.x, self.pos.y, self.pos.z))
+        print("<Camera pos=[%s, %s, %s] target=[%s, %s, %s]>" %
+              (self.pos.x, self.pos.y, self.pos.z,
+               self.looktarget.x, self.looktarget.y, self.looktarget.z))
