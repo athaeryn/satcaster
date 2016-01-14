537c3e7c309267a1be129d9e163de7f25edb676c
Mike Anderson <hello@mrmikea.com>
2016-01-14 01:41:05 -0600

    Add a Vec2 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fmt;
+
+struct Vec2<T> {
+    x: T,
+    y: T
+}
+
+impl<T: fmt::Display> fmt::Display for Vec2<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<Vec2 x={} y={}>", self.x, self.y)
+    }
+}
+
 fn main() {
-    println!("Hello again, Satcaster!");
+    let v = Vec2 { x: 1.23f32, y: 4.56f32 };
+    println!("Hello again, Satcaster!\n{}", v);
 }
