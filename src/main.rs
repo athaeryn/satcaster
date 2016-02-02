15fa5e675f6cadcda5698a315e9a8d72bdaefee1
Mike Anderson <hello@mrmikea.com>
2016-02-02 07:54:57 -0600

    Add deps, get running w/SDL2 example

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,57 @@
-use std::fmt;
+extern crate cgmath;
+extern crate sdl2;
 
-struct Vec2<T> {
-    x: T,
-    y: T
-}
+use cgmath::{Vector3};
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
-impl<T: fmt::Display> fmt::Display for Vec2<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<Vec2 x={} y={}>", self.x, self.y)
-    }
-}
+const WIDTH: u32 = 500;
+const HEIGHT: u32 = 500;
 
 fn main() {
-    let v = Vec2 { x: 1.23f32, y: 4.56f32 };
-    println!("Hello again, Satcaster!\n{}", v);
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("satcaster", WIDTH, HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer().build().unwrap();
+
+    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (WIDTH, HEIGHT)).unwrap();
+    // Create a red-green gradient
+    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        for y in 0..WIDTH {
+            for x in 0..HEIGHT {
+                let offset = y as usize * pitch + x as usize * 3;
+                buffer[offset + 0] = 0;
+                buffer[offset + 1] = 0;
+                buffer[offset + 2] = 0;
+            }
+        }
+    }).unwrap();
+
+    renderer.clear();
+    renderer.copy(&texture, None, Some(Rect::new_unwrap(0, 0, WIDTH, HEIGHT)));
+    renderer.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+    }
+
+    // let v = Vector3 { x: 1.23f32, y: 4.56f32, z: 7.89f32};
+    // println!("Hello again, Satcaster!\n{:?}", v);
 }
